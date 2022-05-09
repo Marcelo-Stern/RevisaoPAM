b2afe1ec1e29e90466887e11eb293de944d77003
Marcelo-Stern <89087684+Marcelo-Stern@users.noreply.github.com>
2022-05-09 09:10:32 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/apprevisao/MainActivity.kt
+++ app/src/main/java/com/example/apprevisao/MainActivity.kt
@@ -4,33 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.botao).setOnClickListener{
-            comparaNumero()
-        }
-    }
 
-    private fun comparaNumero(){
         val num1 = findViewById<EditText>(R.id.numero1)
         val num2 = findViewById<EditText>(R.id.numero2)
-        val resultado = findViewById<TextView>(R.id.teste)
 
-        if(num1 > num2){
+        findViewById<Button>(R.id.botao).setOnClickListener {
+            val primeiro = num1.text.toString()
+            val segundo = num2.text.toString()
 
-        }
-        if(num1 < num2){
+            if (primeiro == "" && segundo == "") {
+                Toast.makeText(
+                    applicationContext, "Entrada inválida",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                primeiro.toInt();
+                segundo.toInt();
 
-        }
-        if(num1 == num2){
-
-        }
-        else{
-
+                if (primeiro > segundo) {
+                    Toast.makeText(
+                        applicationContext, "O primeiro número é maior",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                if (primeiro < segundo) {
+                    Toast.makeText(
+                        applicationContext, "O segundo número é maior",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                if (primeiro == segundo) {
+                    Toast.makeText(
+                        applicationContext, "Os dois números são iguais",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        applicationContext, "Entrada inválida",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 }
